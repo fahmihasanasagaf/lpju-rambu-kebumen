0886f2fc16a4d06383c57c939c1903e1cf49babd
fahmihasanasagaf <sayafahmi18@gmail.com>
2026-09-10 10:05:19 +0700

    revisi role

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,14 +19,14 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
-            'role' => 'nullable|in:admin,operator,user',
+            'role' => 'nullable|in:admin,operator',
         ]);
 
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role' => $data['role'] ?? 'user',
+            'role' => $data['role'] ?? 'operator',
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
--- database/migrations/2026_09_08_025841_add_role_to_users_table.php
+++ database/migrations/2026_09_08_025841_add_role_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-         Schema::table('users', function (Blueprint $table) {
+         Schema::table('operators', function (Blueprint $table) {
         $table->string('role')->default('user')->after('email');
     });
     }
